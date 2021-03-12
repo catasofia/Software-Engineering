779cf07c1847b1eaa56a013bd16d0a498e843b0c
nelsontr <trindade.nelson99@gmail.com>
2021-03-12 10:02:12 +0000

    F2.1 Create question with more than one correct option #38
    F2.1 Create question with more than one correct option - Define test cases #8
    
    F2.1 Create question with more than one correct option - Implement test cases #30
    
    F2.1 Create question with more than one correct option - Implement the service #32
    
    F2.1 Create question with more than one correct option - Refactor the service and test cases #35

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/answer/dto/MultipleChoiceCorrectAnswerDto.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/answer/dto/MultipleChoiceCorrectAnswerDto.java
@@ -1,19 +1,22 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.answer.dto;
 
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.MultipleChoiceQuestion;
+import java.util.List;
+import java.util.ArrayList;
+
 
 public class MultipleChoiceCorrectAnswerDto extends CorrectAnswerDetailsDto {
-    private Integer correctOptionId;
+    private List<Integer> correctOptionId;
 
     public MultipleChoiceCorrectAnswerDto(MultipleChoiceQuestion question) {
-        this.correctOptionId = question.getCorrectOptionId();
+        this.correctOptionId = question.getCorrectOptionsId();
     }
 
-    public Integer getCorrectOptionId() {
+    public List<Integer> getCorrectOptionId() {
         return correctOptionId;
     }
 
-    public void setCorrectOptionId(Integer correctOptionId) {
+    public void setCorrectOptionId(List<Integer> correctOptionId) {
         this.correctOptionId = correctOptionId;
     }
 
