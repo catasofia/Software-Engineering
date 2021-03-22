f7fcb3926602731825f9db7091241b5635480d35
Nelson Trindade <48919500+nelsontr@users.noreply.github.com>
2021-03-22 15:43:46 +0000

    Pem - F2.1 Create question with more than one correct option (#38)
    * F2.1 Create question with more than one correct option - Define test cases #8
    
    * F2.1 Create question with more than one correct option - Implement test cases #30
    
    * F2.1 Create question with more than one correct option - Implement the service #32
    
    * F2.1 Create question with more than one correct option - Refactor the service and test cases #35
    
    Co-authored-by: catasofia <16.catarina.sousa@gmail.com>

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/service/CreateQuestionTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/service/CreateQuestionTest.groovy
@@ -396,6 +396,297 @@ class CreateQuestionTest extends SpockTest {
         exception.getErrorMessage() == ErrorMessage.AT_LEAST_THREE_SLOTS_NEEDED
     }
 
+    def "create a multiple choice question with no image and two correct option"() {
+        given: "a questionDto"
+        def questionDto = new QuestionDto()
+        questionDto.setKey(1);
+        questionDto.setTitle(QUESTION_1_TITLE)
+        questionDto.setContent(QUESTION_1_CONTENT)
+        questionDto.setStatus(Question.Status.AVAILABLE.name())
+        questionDto.setQuestionDetailsDto(new MultipleChoiceQuestionDto())
+
+        and : 'four options'
+        def optionDto = new OptionDto()
+        optionDto.setContent(OPTION_1_CONTENT)
+        optionDto.setCorrect(true)
+        def options = new ArrayList<OptionDto>()
+        options.add(optionDto)
+
+        optionDto = new OptionDto()
+        optionDto.setContent(OPTION_2_CONTENT)
+        optionDto.setCorrect(false)
+        options.add(optionDto)
+
+        optionDto = new OptionDto()
+        optionDto.setContent(OPTION_3_CONTENT)
+        optionDto.setCorrect(true)
+        options.add(optionDto)
+
+        optionDto = new OptionDto()
+        optionDto.setContent(OPTION_4_CONTENT)
+        optionDto.setCorrect(false)
+        options.add(optionDto)
+
+        questionDto.getQuestionDetailsDto().setOptions(options)
+
+        when:
+        questionService.createQuestion(externalCourse.getId(), questionDto)
+
+        then: "the correct question is inside the repository"
+        questionRepository.count() == 1L
+        def result = questionRepository.findAll().get(0)
+        result.getId() != null
+        result.getKey() == 1
+        result.getStatus() == Question.Status.AVAILABLE
+        result.getTitle() == QUESTION_1_TITLE
+        result.getContent() == QUESTION_1_CONTENT
+        result.getImage() == null
+        result.getQuestionDetails().getOptions().size() == 4
+        result.getCourse().getName() == COURSE_1_NAME
+        externalCourse.getQuestions().contains(result)
+
+        def resOption = result.getQuestionDetails().getOptions().get(0)
+        resOption.getContent() == OPTION_1_CONTENT
+        resOption.isCorrect()
+
+        def resOptionTwo = result.getQuestionDetails().getOptions().get(2)
+        resOptionTwo.getContent() == OPTION_3_CONTENT
+        resOptionTwo.isCorrect()
+    }
+
+    def "create a multiple choice question with no image and three correct option"() {
+        given: "a questionDto"
+        def questionDto = new QuestionDto()
+        questionDto.setKey(1);
+        questionDto.setTitle(QUESTION_1_TITLE)
+        questionDto.setContent(QUESTION_1_CONTENT)
+        questionDto.setStatus(Question.Status.AVAILABLE.name())
+        questionDto.setQuestionDetailsDto(new MultipleChoiceQuestionDto())
+
+        and : 'four options'
+        def optionDto = new OptionDto()
+        optionDto.setContent(OPTION_1_CONTENT)
+        optionDto.setCorrect(true)
+        def options = new ArrayList<OptionDto>()
+        options.add(optionDto)
+
+        optionDto = new OptionDto()
+        optionDto.setContent(OPTION_2_CONTENT)
+        optionDto.setCorrect(false)
+        options.add(optionDto)
+
+        optionDto = new OptionDto()
+        optionDto.setContent(OPTION_3_CONTENT)
+        optionDto.setCorrect(true)
+        options.add(optionDto)
+
+        optionDto = new OptionDto()
+        optionDto.setContent(OPTION_4_CONTENT)
+        optionDto.setCorrect(true)
+        options.add(optionDto)
+
+        questionDto.getQuestionDetailsDto().setOptions(options)
+
+        when:
+        questionService.createQuestion(externalCourse.getId(), questionDto)
+
+        then: "the correct question is inside the repository"
+        questionRepository.count() == 1L
+        def result = questionRepository.findAll().get(0)
+        result.getId() != null
+        result.getKey() == 1
+        result.getStatus() == Question.Status.AVAILABLE
+        result.getTitle() == QUESTION_1_TITLE
+        result.getContent() == QUESTION_1_CONTENT
+        result.getImage() == null
+        result.getQuestionDetails().getOptions().size() == 4
+        result.getCourse().getName() == COURSE_1_NAME
+        externalCourse.getQuestions().contains(result)
+
+        def resOption = result.getQuestionDetails().getOptions().get(0)
+        resOption.getContent() == OPTION_1_CONTENT
+        resOption.isCorrect()
+
+        def resOptionTwo = result.getQuestionDetails().getOptions().get(2)
+        resOptionTwo.getContent() == OPTION_3_CONTENT
+        resOptionTwo.isCorrect()
+
+        def resOptionThree = result.getQuestionDetails().getOptions().get(3)
+        resOptionThree.getContent() == OPTION_4_CONTENT
+        resOptionThree.isCorrect()
+    }
+
+    def "create a multiple choice question with an image and two correct option"() {
+        given: "a questionDto"
+        def questionDto = new QuestionDto()
+        questionDto.setKey(1);
+        questionDto.setTitle(QUESTION_1_TITLE)
+        questionDto.setContent(QUESTION_1_CONTENT)
+        questionDto.setStatus(Question.Status.AVAILABLE.name())
+        questionDto.setQuestionDetailsDto(new MultipleChoiceQuestionDto())
+
+        and: 'an image'
+        def image = new ImageDto()
+        image.setUrl(IMAGE_1_URL)
+        image.setWidth(20)
+        questionDto.setImage(image)
+
+        and : 'four options'
+        def optionDto = new OptionDto()
+        optionDto.setContent(OPTION_1_CONTENT)
+        optionDto.setCorrect(true)
+        def options = new ArrayList<OptionDto>()
+        options.add(optionDto)
+
+        optionDto = new OptionDto()
+        optionDto.setContent(OPTION_2_CONTENT)
+        optionDto.setCorrect(false)
+        options.add(optionDto)
+
+        optionDto = new OptionDto()
+        optionDto.setContent(OPTION_3_CONTENT)
+        optionDto.setCorrect(true)
+        options.add(optionDto)
+
+        optionDto = new OptionDto()
+        optionDto.setContent(OPTION_4_CONTENT)
+        optionDto.setCorrect(false)
+        options.add(optionDto)
+
+        questionDto.getQuestionDetailsDto().setOptions(options)
+
+        when:
+        questionService.createQuestion(externalCourse.getId(), questionDto)
+
+        then: "the correct question is inside the repository"
+        questionRepository.count() == 1L
+        def result = questionRepository.findAll().get(0)
+        result.getId() != null
+        result.getKey() == 1
+        result.getStatus() == Question.Status.AVAILABLE
+        result.getTitle() == QUESTION_1_TITLE
+        result.getContent() == QUESTION_1_CONTENT
+        result.getImage().getId() != null
+        result.getImage().getUrl() == IMAGE_1_URL
+        result.getImage().getWidth() == 20
+        result.getQuestionDetails().getOptions().size() == 4
+        result.getCourse().getName() == COURSE_1_NAME
+        externalCourse.getQuestions().contains(result)
+
+        def resOption = result.getQuestionDetails().getOptions().get(0)
+        resOption.getContent() == OPTION_1_CONTENT
+        resOption.isCorrect()
+
+        def resOptionTwo = result.getQuestionDetails().getOptions().get(2)
+        resOptionTwo.getContent() == OPTION_3_CONTENT
+        resOptionTwo.isCorrect()
+    }
+
+    def "create a multiple choice question with an image and three correct option"() {
+        given: "a questionDto"
+        def questionDto = new QuestionDto()
+        questionDto.setKey(1);
+        questionDto.setTitle(QUESTION_1_TITLE)
+        questionDto.setContent(QUESTION_1_CONTENT)
+        questionDto.setStatus(Question.Status.AVAILABLE.name())
+        questionDto.setQuestionDetailsDto(new MultipleChoiceQuestionDto())
+
+        and: 'an image'
+        def image = new ImageDto()
+        image.setUrl(IMAGE_1_URL)
+        image.setWidth(20)
+        questionDto.setImage(image)
+
+        and : 'four options'
+        def optionDto = new OptionDto()
+        optionDto.setContent(OPTION_1_CONTENT)
+        optionDto.setCorrect(true)
+        def options = new ArrayList<OptionDto>()
+        options.add(optionDto)
+
+        optionDto = new OptionDto()
+        optionDto.setContent(OPTION_2_CONTENT)
+        optionDto.setCorrect(false)
+        options.add(optionDto)
+
+        optionDto = new OptionDto()
+        optionDto.setContent(OPTION_3_CONTENT)
+        optionDto.setCorrect(true)
+        options.add(optionDto)
+
+        optionDto = new OptionDto()
+        optionDto.setContent(OPTION_4_CONTENT)
+        optionDto.setCorrect(true)
+        options.add(optionDto)
+
+        questionDto.getQuestionDetailsDto().setOptions(options)
+
+        when:
+        questionService.createQuestion(externalCourse.getId(), questionDto)
+
+        then: "the correct question is inside the repository"
+        questionRepository.count() == 1L
+        def result = questionRepository.findAll().get(0)
+        result.getId() != null
+        result.getKey() == 1
+        result.getStatus() == Question.Status.AVAILABLE
+        result.getTitle() == QUESTION_1_TITLE
+        result.getContent() == QUESTION_1_CONTENT
+        result.getImage().getId() != null
+        result.getImage().getUrl() == IMAGE_1_URL
+        result.getImage().getWidth() == 20
+        result.getQuestionDetails().getOptions().size() == 4
+        result.getCourse().getName() == COURSE_1_NAME
+        externalCourse.getQuestions().contains(result)
+
+        def resOption = result.getQuestionDetails().getOptions().get(0)
+        resOption.getContent() == OPTION_1_CONTENT
+        resOption.isCorrect()
+
+        def resOptionTwo = result.getQuestionDetails().getOptions().get(2)
+        resOptionTwo.getContent() == OPTION_3_CONTENT
+        resOptionTwo.isCorrect()
+
+        def resOptionThree = result.getQuestionDetails().getOptions().get(3)
+        resOptionThree.getContent() == OPTION_4_CONTENT
+        resOptionThree.isCorrect()
+    }
+
+    def "create a multiple choice question with no image and no correct options"(){
+        given: "a questionDto"
+        def questionDto = new QuestionDto()
+        questionDto.setKey(1);
+        questionDto.setTitle(QUESTION_1_TITLE)
+        questionDto.setContent(QUESTION_1_CONTENT)
+        questionDto.setStatus(Question.Status.AVAILABLE.name())
+        questionDto.setQuestionDetailsDto(new MultipleChoiceQuestionDto())
+
+        and : 'three options'
+        def optionDto = new OptionDto()
+        optionDto.setContent(OPTION_1_CONTENT)
+        optionDto.setCorrect(false)
+        def options = new ArrayList<OptionDto>()
+        options.add(optionDto)
+
+        optionDto = new OptionDto()
+        optionDto.setContent(OPTION_2_CONTENT)
+        optionDto.setCorrect(false)
+        options.add(optionDto)
+
+        optionDto = new OptionDto()
+        optionDto.setContent(OPTION_3_CONTENT)
+        optionDto.setCorrect(false)
+        options.add(optionDto)
+
+        questionDto.getQuestionDetailsDto().setOptions(options)
+
+        when:
+        questionService.createQuestion(externalCourse.getId(), questionDto)
+
+        then: "exception is thrown"
+        def exception = thrown(TutorException);
+        exception.getErrorMessage() == ErrorMessage.NO_CORRECT_OPTION;
+    }
 
     @Unroll
     def "fail to create any question for invalid/non-existent course (#nonExistentId)"(Integer nonExistentId) {
