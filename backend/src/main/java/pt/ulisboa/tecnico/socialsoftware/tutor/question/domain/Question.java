d007e495e95f0c5f7c59ea7d6b67cba5c4a4ec2d
catasofia <16.catarina.sousa@gmail.com>
2021-03-22 15:54:54 +0000

    F2.2 Update question
    F2.2 Update question - Define test cases #36
    
    F2.2 Update question - Implement test cases #37
    
    F2.2 Update question - Implement the service #41
    
    F2.2 Update question - Refactor service and test cases #43

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/domain/Question.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/domain/Question.java
@@ -270,6 +270,7 @@ public class Question implements DomainEntity {
 
         setTitle(questionDto.getTitle());
         setContent(questionDto.getContent());
+        setNumberOfCorrect(questionDto.getNumberOfCorrect());
 
         getQuestionDetails().update(questionDto.getQuestionDetailsDto());
     }
