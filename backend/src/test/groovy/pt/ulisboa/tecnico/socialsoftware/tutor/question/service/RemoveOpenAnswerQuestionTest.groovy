a1c00efcd63caddcd0934f80f7302357c2fe0a18
pipamalta <56034005+pipamalta@users.noreply.github.com>
2021-03-23 14:43:59 +0000

    F1.3 Remove Open Answer Question  (#52)
    * F1.3 Remove Open Answer Question - Define test cases #16
    
    * F1.3 Remove Open Answer Question - Implement test cases #17
    
    * F1.3 Remove Open Answer Question - Implement service #18

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/service/RemoveOpenAnswerQuestionTest.groovy
@@ -0,0 +1,98 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.question.service
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import pt.ulisboa.tecnico.socialsoftware.tutor.BeanConfiguration
+import pt.ulisboa.tecnico.socialsoftware.tutor.SpockTest
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Image
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.MultipleChoiceQuestion
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.OpenAnswerQuestion
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Option
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Topic
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.TopicDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.questionsubmission.domain.QuestionSubmission
+import pt.ulisboa.tecnico.socialsoftware.tutor.quiz.domain.Quiz
+import pt.ulisboa.tecnico.socialsoftware.tutor.quiz.domain.QuizQuestion
+import pt.ulisboa.tecnico.socialsoftware.tutor.auth.domain.AuthUser
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.User
+
+@DataJpaTest
+class RemoveOpenAnswerQuestionTest extends SpockTest {
+
+    def question
+
+    def setup() {
+
+        question = new Question()
+        question.setKey(1)
+        question.setTitle(QUESTION_1_TITLE)
+        question.setContent(QUESTION_1_CONTENT)
+        question.setStatus(Question.Status.AVAILABLE)
+        question.setCourse(externalCourse)
+
+        def questionDetails = new OpenAnswerQuestion()
+        question.setQuestionDetails(questionDetails)
+        questionDetailsRepository.save(questionDetails)
+        questionRepository.save(question)
+
+    }
+
+    def "remove a question"() {
+        when:
+        questionService.removeQuestion(question.getId())
+
+        then: "the question is removeQuestion"
+        questionRepository.count() == 0L
+    }
+
+    def "remove a question used in a quiz"() {
+        given: "a question with answers"
+        Quiz quiz = new Quiz()
+        quiz.setKey(1)
+        quiz.setTitle(QUIZ_TITLE)
+        quiz.setType(Quiz.QuizType.PROPOSED.toString())
+        quiz.setAvailableDate(LOCAL_DATE_BEFORE)
+        quiz.setCourseExecution(externalCourseExecution)
+        quiz.setOneWay(true)
+        quizRepository.save(quiz)
+
+        QuizQuestion quizQuestion= new QuizQuestion()
+        quizQuestion.setQuiz(quiz)
+        quizQuestion.setQuestion(question)
+        quizQuestionRepository.save(quizQuestion)
+
+        when:
+        questionService.removeQuestion(question.getId())
+
+        then: "the question an exception is thrown"
+        def exception = thrown(TutorException)
+        exception.getErrorMessage() == ErrorMessage.QUESTION_IS_USED_IN_QUIZ
+    }
+
+    def "remove a question that was submitted"() {
+        given: "a student"
+        def student = new User(USER_1_NAME, USER_1_USERNAME, USER_1_EMAIL,
+                User.Role.STUDENT, false, AuthUser.Type.TECNICO)
+        userRepository.save(student)
+
+        and: "a questionSubmission"
+        def questionSubmission = new QuestionSubmission()
+        questionSubmission.setQuestion(question)
+        questionSubmission.setSubmitter(student)
+        questionSubmission.setCourseExecution(externalCourseExecution)
+        questionSubmissionRepository.save(questionSubmission)
+
+        when:
+        questionService.removeQuestion(question.getId())
+
+        then: "an exception is thrown"
+        def exception = thrown(TutorException)
+        exception.getErrorMessage() == ErrorMessage.CANNOT_DELETE_SUBMITTED_QUESTION
+    }
+
+    @TestConfiguration
+    static class LocalBeanConfiguration extends BeanConfiguration {}
+}
