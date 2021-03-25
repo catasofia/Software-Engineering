aa9b2bd8ebaee7aa5a75466bd3cdcd5aba5b9312
amariaribeiro <ist193735@tecnico.ulisboa.pt>
2021-03-25 17:49:58 +0000

    F3.4 Item Combination - Export Questions to Xml - Define test cases #77

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/impexp/service/ImportExportItemCombinationQuestionsTest.groovy
@@ -0,0 +1,21 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.impexp.service
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import pt.ulisboa.tecnico.socialsoftware.tutor.BeanConfiguration
+import pt.ulisboa.tecnico.socialsoftware.tutor.SpockTest
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.ImageDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.OptionDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.QuestionDto
+
+@DataJpaTest
+class ImportExportItemCombinationQuestion extends SpockTest {
+	
+	def "export questions to xml" () {
+        expect: false
+    }
+
+    @TestConfiguration
+    static class LocalBeanConfiguration extends BeanConfiguration {}
+}
