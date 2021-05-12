8e0b76affb406a21328d558316644331e709d65e
Jonyleo <joao.ferreira.oliveira@hotmail.com>
2021-05-12 15:08:39 +0100

    F10.3 Teacher See Results Open Answer Question (Quizz Answering, JMeter Testing, Profiling)

--- frontend/src/models/management/questions/OpenAnswerAnswerDetails.ts
+++ frontend/src/models/management/questions/OpenAnswerAnswerDetails.ts
@@ -1,17 +1,23 @@
 import AnswerDetails from '@/models/management/questions/AnswerDetails';
 import { QuestionTypes } from '@/services/QuestionHelpers';
 import QuestionDetails from '@/models/management/questions/QuestionDetails';
+import OpenAnswerQuestionDetails from '@/models/management/questions/OpenAnswerQuestionDetails';
 
 export default class OpenAnswerAnswerDetails extends AnswerDetails {
+  answer!: string;
+
   constructor(jsonObj?: OpenAnswerAnswerDetails) {
     super(QuestionTypes.OpenAnswer);
+    if (jsonObj) {
+      this.answer = jsonObj.answer;
+    }
   }
 
   answerRepresentation(question: QuestionDetails): string {
-    return '';
+    return '-';
   }
 
-  isCorrect(question: QuestionDetails): boolean {
-    return false;
+  isCorrect(question: OpenAnswerQuestionDetails): boolean {
+    return this.answer === question.suggestion;
   }
 }
